8ca096d308098ecf28f47d6f5284ab1f0e32cb69
Soham Pahari <105543476+suhanpahari@users.noreply.github.com>
2023-07-20 10:57:46 +0530

    Add files via upload
    This is a JavaScript file that uses the Fullscreen API to enable fullscreen mode for an element on a web page.
    and the html file is the use of the js.

---
+++ fullsc/fullscreen.js
@@ -0,0 +1,17 @@
+function toggleFullscreen() {
+    const elem = document.documentElement;
+  
+    if (document.fullscreenElement || document.webkitFullscreenElement) {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      }
+    } else {
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+      }
+    }
+  }
